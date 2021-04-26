6e0413c531bd9e484ee8233bbb6b1dcf274e3759
mridle582 <mridle582@gmail.com>
2021-04-26 03:02:07 -0400

    Made dah man

--- FrontEnd/drag.py
+++ FrontEnd/drag.py
@@ -1,7 +1,7 @@
 # drag.py
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-# from matplotlib.lines import Line2D
+from matplotlib.lines import Line2D
 
 
 class DraggablePoint:
@@ -10,24 +10,29 @@ class DraggablePoint:
 
     lock = None #  only one can be animated at a time
 
-    def __init__(self, parent, x=0.1, y=0.1, size=0.1):
+    def __init__(self, parent, body_part, x=0.1, y=0.1, size=0.1):
 
         self.parent = parent
-        self.point = patches.Ellipse((x, y), size, size * 1, fc='b', alpha=0.5, edgecolor='w')
+        self.point = patches.Ellipse((x, y), size, size, fc='r', alpha=0.5, edgecolor='r') if body_part != 'head' else patches.Ellipse((x, y), 2*size, 2*size, fc='r', alpha=0.5, edgecolor='r')
         self.x = x
         self.y = y
         parent.fig.axes[0].add_patch(self.point)
         self.press = None
         self.background = None
+        self.body_part = body_part
         self.connect()
         
-        # # if another point already exist we draw a line
-        # if self.parent.list_points:
-        #     line_x = [self.parent.list_points[-1].x, self.x]
-        #     line_y = [self.parent.list_points[-1].y, self.y]
+        # if another point already exist we draw a line
+        if self.parent.list_points[body_part]:
+            line_x = [self.parent.list_points[body_part][-1].x, self.x]
+            line_y = [self.parent.list_points[body_part][-1].y, self.y]
 
-        #     self.line = Line2D(line_x, line_y, color='r', alpha=0.5)
-        #     parent.fig.axes[0].add_line(self.line)
+            self.line = Line2D(line_x, line_y, color='r', alpha=0.5)
+            parent.fig.axes[0].add_line(self.line)
+
+        elif self.body_part == 'head':
+            self.line = Line2D([0,0], [0,0], color='r', alpha=0.5)
+            parent.fig.axes[0].add_line(self.line)
 
 
     def connect(self):
@@ -54,15 +59,18 @@ class DraggablePoint:
         self.point.set_animated(True)
         
         # TODO also the line of some other points needs to be released
-        point_number =  self.parent.list_points.index(self)
+        point_number =  self.parent.list_points[self.body_part].index(self)
+
+        if self.body_part == 'head':
+            self.line.set_animated(True)
         
-        if self == self.parent.list_points[0]:
-            self.parent.list_points[1].line.set_animated(True)            
-        elif self == self.parent.list_points[-1]:
+        elif self == self.parent.list_points[self.body_part][0]:
+            self.parent.list_points[self.body_part][1].line.set_animated(True)            
+        elif self == self.parent.list_points[self.body_part][-1]:
             self.line.set_animated(True)            
         else:
             self.line.set_animated(True)            
-            self.parent.list_points[point_number+1].line.set_animated(True)                
+            self.parent.list_points[self.body_part][point_number+1].line.set_animated(True)                
             
             
             
@@ -89,59 +97,60 @@ class DraggablePoint:
 
         canvas = self.point.figure.canvas
         axes = self.point.axes
+
         # restore the background region
         canvas.restore_region(self.background)
 
         # redraw just the current rectangle
         axes.draw_artist(self.point)
         
-        point_number =  self.parent.list_points.index(self)
+        point_number =  self.parent.list_points[self.body_part].index(self)
         self.x = self.point.center[0]
         self.y = self.point.center[1]
         
-        
-        
-        
+
+        if self.body_part == 'head':
+            axes.draw_artist(self.line)    
         
         # We check if the point is A or B        
-        if self == self.parent.list_points[0]:
+        elif self == self.parent.list_points[self.body_part][0]:
             # or we draw the other line of the point
-            self.parent.list_points[1].line.set_animated(True)
-            axes.draw_artist(self.parent.list_points[1].line)
+            self.parent.list_points[self.body_part][1].line.set_animated(True)
+            axes.draw_artist(self.parent.list_points[self.body_part][1].line)
         
-        elif self == self.parent.list_points[-1]:
+        elif self == self.parent.list_points[self.body_part][-1]:
             # we draw the line of the point            
             axes.draw_artist(self.line)    
 
         else:
             # we draw the line of the point
             axes.draw_artist(self.line)
-            #self.parent.list_points[point_number+1].line.set_animated(True)
-            axes.draw_artist(self.parent.list_points[point_number+1].line)
+            #self.parent.list_points[self.body_part][point_number+1].line.set_animated(True)
+            axes.draw_artist(self.parent.list_points[self.body_part][point_number+1].line)
             
-                
         
-        
-        if self == self.parent.list_points[0]:
+        if self.body_part == 'head':
+            print('moved head')
+        elif self == self.parent.list_points[self.body_part][0]:
             # The first point is especial because it has no line
-            line_x = [self.x, self.parent.list_points[1].x]
-            line_y = [self.y, self.parent.list_points[1].y]      
+            line_x = [self.x, self.parent.list_points[self.body_part][1].x]
+            line_y = [self.y, self.parent.list_points[self.body_part][1].y]      
             # this is were the line is updated
-            self.parent.list_points[1].line.set_data(line_x, line_y)
+            self.parent.list_points[self.body_part][1].line.set_data(line_x, line_y)
             
-        elif self == self.parent.list_points[-1]:
-            line_x = [self.parent.list_points[-2].x, self.x]
-            line_y = [self.parent.list_points[-2].y, self.y]
+        elif self == self.parent.list_points[self.body_part][-1]:
+            line_x = [self.parent.list_points[self.body_part][-2].x, self.x]
+            line_y = [self.parent.list_points[self.body_part][-2].y, self.y]
             self.line.set_data(line_x, line_y)        
         else:
             # The first point is especial because it has no line
-            line_x = [self.x, self.parent.list_points[point_number+1].x]
-            line_y = [self.y, self.parent.list_points[point_number+1].y]      
+            line_x = [self.x, self.parent.list_points[self.body_part][point_number+1].x]
+            line_y = [self.y, self.parent.list_points[self.body_part][point_number+1].y]      
             # this is were the line is updated
-            self.parent.list_points[point_number+1].line.set_data(line_x, line_y)
+            self.parent.list_points[self.body_part][point_number+1].line.set_data(line_x, line_y)
             
-            line_x = [self.parent.list_points[point_number-1].x, self.x]
-            line_y = [self.parent.list_points[point_number-1].y, self.y]
+            line_x = [self.parent.list_points[self.body_part][point_number-1].x, self.x]
+            line_y = [self.parent.list_points[self.body_part][point_number-1].y, self.y]
             self.line.set_data(line_x, line_y)        
 
         # blit just the redrawn area
@@ -160,15 +169,18 @@ class DraggablePoint:
         # turn off the rect animation property and reset the background
         self.point.set_animated(False)
         
-        point_number =  self.parent.list_points.index(self)
-        
-        if self == self.parent.list_points[0]:
-            self.parent.list_points[1].line.set_animated(False)            
-        elif self == self.parent.list_points[-1]:
+        point_number =  self.parent.list_points[self.body_part].index(self)
+
+        if self.body_part == 'head':
+            self.line.set_animated(False) 
+
+        elif self == self.parent.list_points[self.body_part][0]:
+            self.parent.list_points[self.body_part][1].line.set_animated(False)            
+        elif self == self.parent.list_points[self.body_part][-1]:
             self.line.set_animated(False)            
         else:
             self.line.set_animated(False)            
-            self.parent.list_points[point_number+1].line.set_animated(False)       
+            self.parent.list_points[self.body_part][point_number+1].line.set_animated(False)       
             
 
         self.background = None
--- FrontEnd/figure.py
+++ FrontEnd/figure.py
@@ -1,97 +1,83 @@
 import sys
 import matplotlib
 matplotlib.use("Qt5Agg")
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets
+
+# Personnal modules
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from matplotlib.lines import Line2D
+
 
 # Personnal modules
 from drag import DraggablePoint
 
-
 class MyGraph(FigureCanvas):
 
-    def __init__(self, parent=None, width=8, height=8, dpi=120): 
-        bgcol = parent.palette().window().color().toRgb() if parent is not None else "#000000" 
-        bgcol = [bgcol.redF(), bgcol.greenF(), bgcol.blueF()] if parent is not None else "#000000"
+    """A canvas that updates itself every second with a new plot."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+
         self.fig = Figure(figsize=(width, height), dpi=dpi)
-        self.fig.set_facecolor(bgcol)
-        self.axes = self.fig.add_subplot(111)   
-        self.axes.grid(False)    
-        self.axes.get_xaxis().set_visible(False)
-        self.axes.get_yaxis().set_visible(False)
+        self.axes = self.fig.add_subplot(111)
+
+        self.axes.grid(True)
+
         FigureCanvas.__init__(self, self.fig)
-        self.setParent(parent)  
+        self.setParent(parent)
+
         FigureCanvas.setSizePolicy(self,
                                    QtWidgets.QSizePolicy.Expanding,
                                    QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)   
-        # To store the 2 draggable points
-        self.list_points = [None] * 6
+        FigureCanvas.updateGeometry(self)
+
+        # To store draggable points
+        self.list_points = {'left': [], 'head': [], 'right': []}
+
+
         self.show()
-        self.plot_draggable_points()    
+        self.plotDraggablePoints()
 
-    def plot_draggable_points(self, size=0.05):
 
-        # # if another point already exist we draw a line
-        # if self.parent.list_points:
-        #     line_x = [self.parent.list_points[-1].x, self.x]
-        #     line_y = [self.parent.list_points[-1].y, self.y]
+    def plotDraggablePoints(self, size=0.05):
 
-        #     self.line = Line2D(line_x, line_y, color='r', alpha=0.5)
-        #     parent.fig.axes[0].add_line(self.line)
-        # self.list_points[0] = DraggablePoint(self, 0.4, 0.1, size)
-        # self.list_points[1] = DraggablePoint(self, 0.4, 0.27, size)
-        # self.list_points[2] = DraggablePoint(self, 0.4, 0.44, size)
-        # self.list_points[3] = DraggablePoint(self, 0.4, 0.85, size)
-        # self.list_points[4] = DraggablePoint(self, 0.25, 0.67, size)
-        # self.list_points[5] = DraggablePoint(self, 0.125, 0.67, size)
+        """Plot and define the 2 draggable points of the baseline"""
+  
+        self.list_points['left'].append(DraggablePoint(self, 'left', 0.2, 0.35, size))
+        self.list_points['left'].append(DraggablePoint(self, 'left', 0.3, 0.5, size))
+        self.list_points['left'].append(DraggablePoint(self, 'left', 0.4, 0.65, size))
+        self.list_points['left'].append(DraggablePoint(self, 'left', 0.4, 0.40, size))
+        self.list_points['left'].append(DraggablePoint(self, 'left', 0.4, 0.25, size))
+        self.list_points['left'].append(DraggablePoint(self, 'left', 0.4, 0.10, size))
 
-        top_left = self.list_points[0] = DraggablePoint(self, 0.4, 0.85, size)
-        top_right = self.list_points[1] = DraggablePoint(self, 0.6, 0.85, size)
-        bottom_right = self.list_points[2] = DraggablePoint(self, 0.6, 0.44, size)
-        #bottom_left = self.list_points[3] = DraggablePoint(self, 0.4, 0.44, size)
+        self.list_points['head'].append(DraggablePoint(self, 'head', 0.5, 0.75, size))
 
-        top_right.line = Line2D([top_left.x, top_right.x],[top_left.y, top_right.y], color='r', alpha=0.5)
-        self.fig.axes[0].add_line(top_right.line)
-
-        bottom_right.line = Line2D([top_right.x, bottom_right.x], [top_right.y, bottom_right.y], color='r', alpha=0.5)
-        self.fig.axes[0].add_line(bottom_right.line)
-
-        top_left.line = Line2D([bottom_right.x, top_left.x],[bottom_right.y, top_left.y], color='r', alpha=0.5)
-        self.fig.axes[0].add_line(top_left.line)
-
-        # top_left.line = Line2D([bottom_left.x, top_left.x],[bottom_left.y, top_left.y], color='r', alpha=0.5)
-        # self.fig.axes[0].add_line(top_left.line)
+        self.list_points['right'].append(DraggablePoint(self, 'right', 0.8, 0.35, size))
+        self.list_points['right'].append(DraggablePoint(self, 'right', 0.7, 0.5, size))
+        self.list_points['right'].append(DraggablePoint(self, 'right', 0.6, 0.65, size))
+        self.list_points['right'].append(DraggablePoint(self, 'right', 0.6, 0.40, size))
+        self.list_points['right'].append(DraggablePoint(self, 'right', 0.6, 0.25, size))
+        self.list_points['right'].append(DraggablePoint(self, 'right', 0.6, 0.10, size))
 
 
 
-
-
-        self.updateFigure()
-
-
-    def clearFigure(self):
-
-        self.axes.clear()
-        self.axes.grid(False)
-        del(self.list_points[:])
         self.updateFigure()
 
 
     def updateFigure(self):
+
+        """Update the graph. Necessary, to call after each plot"""
+
         self.draw()
 
-if __name__ == '__main__':
 
-    
-    app = QtWidgets.QApplication(sys.argv)
-    temp =MyGraph()
-    # mw = QtWidgets.QMainWindow()
-    # mw.setStyleSheet("background-color:red")
-    # mw.setGeometry(0, 0, 300, 300)
-    # mw.setCentralWidget(temp)
-    
+
+if __name__ == '__main__':
+    app = QApplication([])
+    win = QWidget()
+    layout = QGridLayout()
+    layout.addWidget(MyGraph())
+    win.setLayout(layout)
+    win.show()
     sys.exit(app.exec_())
